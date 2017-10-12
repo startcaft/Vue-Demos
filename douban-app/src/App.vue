f7b04a4198c58b80c7e6e73b524a3f9e06b1f253
startcaft <51418996@qq.com>
2017-10-12 11:37:20 +0800

    将底部导航栏放在App.vue根组件上

--- douban-app/src/App.vue
+++ douban-app/src/App.vue
@@ -1,23 +1,64 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
+    <!--
+          v-mode绑定的属性值将传递到子组件定义的value属性上
+          
+          v-bind:value="select"
+          v-on:input="select = $event.target.value"
+      -->
+      <m-tabbar v-model="select">
+          <!--以下的内容分发给m-tabbar组件的默认slot-->
+          <m-tabbar-item id="tab1">
+              <!--分发给m-tabbar-item组件的具名为icon-normal和icon-active的slot-->
+              <img src="./assets/images/ic_tab_home_normal.png" alt="" slot="icon-normal">
+              <img src="./assets/images/ic_tab_home_active.png" alt="" slot="icon-active">
+              首页
+          </m-tabbar-item>
+          <m-tabbar-item id="tab2">
+              <!--分发给m-tabbar-item组件的具名为icon-normal和icon-active的slot-->
+              <img src="./assets/images/ic_tab_subject_normal.png" alt="" slot="icon-normal">
+              <img src="./assets/images/ic_tab_subject_active.png" alt="" slot="icon-active">
+              书影音
+          </m-tabbar-item>
+          <m-tabbar-item id="tab3">
+              <!--分发给m-tabbar-item组件的具名为icon-normal和icon-active的slot-->
+              <img src="./assets/images/ic_tab_status_normal.png" alt="" slot="icon-normal">
+              <img src="./assets/images/ic_tab_status_active.png" alt="" slot="icon-active">
+              广播
+          </m-tabbar-item>
+          <m-tabbar-item id="tab4">
+              <!--分发给m-tabbar-item组件的具名为icon-normal和icon-active的slot-->
+              <img src="./assets/images/ic_tab_group_normal.png" alt="" slot="icon-normal">
+              <img src="./assets/images/ic_tab_group_active.png" alt="" slot="icon-active">
+              小组
+          </m-tabbar-item>
+          <m-tabbar-item id="tab5">
+              <!--分发给m-tabbar-item组件的具名为icon-normal和icon-active的slot-->
+              <img src="./assets/images/ic_tab_profile_normal.png" alt="" slot="icon-normal">
+              <img src="./assets/images/ic_tab_profile_active.png" alt="" slot="icon-active">
+              我的
+          </m-tabbar-item>
+      </m-tabbar> 
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+    import Tabbar from './components/tabbar.vue'
+    import TabbarItem from './components/tabber-item.vue'
+    export default {
+      name: 'app',
+      components : {
+          'm-tabbar' : Tabbar,
+          'm-tabbar-item' : TabbarItem,
+      },
+      data(){
+          return {
+              select : 'tab1'
+          }
+      }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- douban-app/src/pages/index.vue
+++ douban-app/src/pages/index.vue
@@ -1,64 +1,26 @@
 <template>
-    <!--
-        v-mode绑定的属性值将传递到子组件定义的value属性上
-        
-        v-bind:value="select"
-        v-on:input="select = $event.target.value"
-    -->
-    <m-tabbar v-model="select">
-        <!--以下的内容分发给m-tabbar组件的默认slot-->
-        <m-tabbar-item id="tab1">
-            <!--分发给m-tabbar-item组件的具名为icon-normal和icon-active的slot-->
-            <img src="../assets/images/ic_tab_home_normal.png" alt="" slot="icon-normal">
-            <img src="../assets/images/ic_tab_home_active.png" alt="" slot="icon-active">
-            首页
-        </m-tabbar-item>
-        <m-tabbar-item id="tab2">
-            <!--分发给m-tabbar-item组件的具名为icon-normal和icon-active的slot-->
-            <img src="../assets/images/ic_tab_subject_normal.png" alt="" slot="icon-normal">
-            <img src="../assets/images/ic_tab_subject_active.png" alt="" slot="icon-active">
-            书影音
-        </m-tabbar-item>
-        <m-tabbar-item id="tab3">
-            <!--分发给m-tabbar-item组件的具名为icon-normal和icon-active的slot-->
-            <img src="../assets/images/ic_tab_status_normal.png" alt="" slot="icon-normal">
-            <img src="../assets/images/ic_tab_status_active.png" alt="" slot="icon-active">
-            广播
-        </m-tabbar-item>
-        <m-tabbar-item id="tab4">
-            <!--分发给m-tabbar-item组件的具名为icon-normal和icon-active的slot-->
-            <img src="../assets/images/ic_tab_group_normal.png" alt="" slot="icon-normal">
-            <img src="../assets/images/ic_tab_group_active.png" alt="" slot="icon-active">
-            小组
-        </m-tabbar-item>
-        <m-tabbar-item id="tab5">
-            <!--分发给m-tabbar-item组件的具名为icon-normal和icon-active的slot-->
-            <img src="../assets/images/ic_tab_profile_normal.png" alt="" slot="icon-normal">
-            <img src="../assets/images/ic_tab_profile_active.png" alt="" slot="icon-active">
-            我的
-        </m-tabbar-item>
-    </m-tabbar>
+    <div>
+        <m-header title="豆瓣app" :bg="true">
+            <a href="javascript:;" slot="left">
+                <img class="m-icon-img" src="../assets/images/ic_bar_back_white.png">
+                返回
+            </a>
+            <a href="javascript:;" slot="right">分享</a>
+        </m-header>
+    </div>
 </template>
 
 <script>
-    import Tabbar from '../components/tabbar.vue'
-    import TabbarItem from '../components/tabber-item.vue'
+    import Header from '../components/header.vue'
 
     export default {
         components : {
-            'm-tabbar' : Tabbar,
-            'm-tabbar-item' : TabbarItem
-        },
-        data(){
-            return {
-                select : 'tab1'
-            }
+            'm-header' : Header
         }
     }
 </script>
 
 
 <style lang="less">
-
 </style>
 
