8742970764e1a3f7be5da68c9a66dbafa58d2985
startcaft <51418996@qq.com>
2017-10-12 15:53:30 +0800

    swiper组件测试完毕

---
+++ douban-app/src/components/swiper.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="swiper-container" :class="swipeid">
+        <div class="swiper-wrapper">
+            <slot name="swiper-on"></slot>
+        </div>
+        <!-- 分页器 -->
+        <div :class="{'swiper-pagination':pagination}"></div>
+    </div>
+</template>
+<script>
+    /**
+     * @description swiper轮播
+     * @param swipeid - 轮播组件id
+     * @param effect - 播放时的特效，取值 slide,fade,coverflow,cube
+     * @param loop - 是否循环，默认true
+     * @param direction - 方向，默认水平方向
+     * @param pagination - 是否有分页器
+     * @param autoplay - 轮播延时，默认5000毫秒
+     * @param paginationType - 分页类型，取值 fraction,progress,bullets
+     */
+    import '../assets/lib/swiper/js/swiper.js'
+    export default {
+        props:{
+            swipeid:{
+                type:String,
+                default: 'swipe01'
+            },
+            effect:{
+                type:String,
+                default:'slide'
+            },
+            loop:{
+                type:Boolean,
+                default:true
+            },
+            direction:{
+                type:String,
+                default:'horizontal'
+            },
+            pagination:{
+                type:Boolean,
+                default:true
+            },
+            autoplay: {
+                type: Number,
+                default: 5000,
+            },
+            paginationType: {
+                type: String,
+                default: 'bullets'
+            }
+        },
+        mounted:function(){
+            let that = this;
+            let mySwiper = new Swiper('.' + that.swipeid,{
+                //循环
+                loop:that.loop,
+                //分页器
+                pagination : '.swiper-pagination',
+                //分页类型
+                paginationType: that.paginationType, //fraction,progress,bullets
+                //自动播放
+                autoplay: that.autoplay,
+                //方向
+                direction: that.direction,
+                //特效
+                effect: that.effect, //slide,fade,coverflow,cube
+            })
+        }
+    }
+</script>
+
+<style>
+    @import '../assets/lib/swiper/css/swiper.css';
+    .swiper-pagination-bullet-active {
+        background: #fff;
+    }
+</style>
+
--- douban-app/src/pages/index.vue
+++ douban-app/src/pages/index.vue
@@ -7,20 +7,72 @@
             </a>
             <a href="javascript:;" slot="right">分享</a>
         </m-header>
+        <div class="page-content">
+            <m-swiper swipeid="swipe01" :autoplay="1000" effect="cubu">
+                <div class="swiper-slide slide02" slot="swiper-on">Slide 1</div>
+                <div class="swiper-slide slide01" slot="swiper-on">Slide 2</div>
+                <div class="swiper-slide slide03" slot="swiper-on">Slide 3</div>
+            </m-swiper>
+            <m-swiper swipeid="swipe021" :loop="false" paginationType="fraction" :autoplay="2000">
+                <div class="swiper-slide slide02" slot="swiper-on">Slide 1</div>
+                <div class="swiper-slide slide01" slot="swiper-on">Slide 2</div>
+                <div class="swiper-slide slide03" slot="swiper-on">Slide 3</div>
+            </m-swiper>
+            <p>asdasdas</p>
+            <p>asdasdas</p>
+
+            <p>asdasdas</p>
+            <p>asdasdas</p>
+            <p>asdasdas</p>
+
+            <p>asdasdas</p>
+            <p>asdasdas</p>
+            <p>asdasdas</p>
+            <p>asdasdas</p>
+            <p>asdasdas</p>
+            <p>asdasdas</p>
+            <p>asdasdas</p>
+        </div>
     </div>
 </template>
 
 <script>
     import Header from '../components/header.vue'
+    import Swiper from '../components/swiper.vue'
 
     export default {
         components : {
-            'm-header' : Header
+            'm-header' : Header,
+            'm-swiper' : Swiper
         }
     }
 </script>
 
 
 <style lang="less">
+    .is-fixed ~ .page-content{
+        padding-top:44px;
+    }
+    .slide01{
+        background: #41b883;
+        text-align: center;
+        line-height: 200px;
+        font-size: 30px;
+        color: #fff;
+    }
+    .slide02{
+        background: #364a60;
+        text-align: center;
+        line-height: 200px;
+        font-size: 30px;
+        color: #fff;
+    }
+    .slide03{
+        background: #ea6f5a;
+        text-align: center;
+        line-height: 200px;
+        font-size: 30px;
+        color: #fff;
+    }
 </style>
 
