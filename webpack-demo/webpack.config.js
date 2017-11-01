afc65feacc03fa88c047b52167cce839be3b1779
startcaft <51418996@qq.com>
2017-11-01 10:03:40 +0800

    使用 webpack.config.js 配置文件来进行打包

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,29 @@
+//webpack配置文件，该配置下，终端输入：webpack 即可打包完成
+
+// 引入 nodejs 的 path 模块
+const path = require('path');
+
+module.exports = {
+    // 入口文件的配置项
+    entry:{
+        // 这里的 entryFile 随意
+        entryFile:'./src/entry.js'
+    },
+
+    // 出口文件的配置项
+    output:{
+        // 指定打包的路径
+        path:path.resolve(__dirname,'dist'),
+        // 指定文件名
+        filename:'bundle.js'
+    },
+
+    // 模块：列入解析CSS，图片路径转换等
+    module:{},
+
+    // 插件：特定的功能插件
+    plugins:[],
+
+    // 配置webpack开发服务以及热更新
+    devServer:{}
+}
