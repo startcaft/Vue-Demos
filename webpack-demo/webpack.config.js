cba01448f38c2f9e56983474fff853f0403363fa
startcaft <51418996@qq.com>
2017-11-01 10:34:33 +0800

    安装webpack-dev-server来配置服务和配置项目的热更新

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,4 +1,5 @@
-//webpack配置文件，该配置下，终端输入：webpack 即可打包完成
+//webpack配置文件，该配置下，终端输入：webpack-dev-server 开启服务和热更新，
+//可能，会有报错，最好将 webpack-dev-server 命令配置到package.json的scripts选项中
 
 // 引入 nodejs 的 path 模块
 const path = require('path');
@@ -8,8 +9,6 @@ module.exports = {
     entry:{
         // 这里的 entryFile 随意
         entryFile:'./src/entry.js',
-        // 多入口文件配置
-        entryFile2:'./src/entry2.js'
     },
 
     // 出口文件的配置项
@@ -17,7 +16,7 @@ module.exports = {
         // 指定打包的路径
         path:path.resolve(__dirname,'dist'),
         // 指定文件名
-        filename:'[name].js'
+        filename:'bundle.js'
     },
 
     // 模块：列入解析CSS，图片路径转换等
@@ -27,5 +26,14 @@ module.exports = {
     plugins:[],
 
     // 配置webpack开发服务以及热更新
-    devServer:{}
+    devServer:{
+        // 设置生产环境的基本目录结构
+        contentBase : path.resolve(__dirname,'dist'),
+        // 设置服务器的IP地址
+        host : 'localhost',
+        // 设置服务器的端口
+        port : '8888',
+        // 设置服务端压缩是否开启
+        compress : true
+    }
 }
